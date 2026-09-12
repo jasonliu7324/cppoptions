b066f5624b10dfc96764b7445e3c62928461355d
Jason Liu <jasonliu7324@gmail.com>
2026-09-12 14:30:48 -0400

    even more testing with cmake

--- maintest2.cpp
+++ maintest2.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+#include "Option.hpp"
+#include "EuropeanOption.hpp"
+#include "EuropeanCallOption.hpp"
+#include "EuropeanPutOption.hpp"
+#include "AmericanPerpetualCallOption.hpp"
+#include "AmericanPerpetualPutOption.hpp"
+#include "OptionGlobalFunctions.hpp"
+
 int main()
 {
     std::cout << "test" << std::endl;
