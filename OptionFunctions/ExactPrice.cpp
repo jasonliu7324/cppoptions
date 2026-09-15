cdc05f1535c35af9df45314312488afab1069f1c
jasonliu7324 <jasonliu7324@gmail.com>
2026-09-15 15:49:35 -0400

    Implemented ExactPrice class

--- OptionFunctions/ExactPrice.cpp
+++ OptionFunctions/ExactPrice.cpp
@@ -3,3 +3,54 @@
 //
 //
 //
+
+#include "ExactPrice.hpp"
+#include "Option.hpp"
+
+namespace Jason::Finance
+{
+    // Base and copy constructors
+    ExactPrice::ExactPrice() : PricingMethod() {};
+    ExactPrice::ExactPrice(const ExactPrice& source) : PricingMethod(source) {};
+
+    // Assignment operator
+    ExactPrice& ExactPrice::operator = (const ExactPrice& source)
+    {
+        if (this == &source)
+        {
+            return *this;
+        }
+
+        PricingMethod::operator = (source);
+
+        return *this;
+    }
+
+    // Destructor
+    ExactPrice::~ExactPrice() {};
+
+    // Name
+    std::string ExactPrice::name() const
+    {
+        return "Exact Price";
+    }
+
+    // isAvailable
+    bool ExactPrice::isAvailable(const Option& opt) const
+    {
+        return opt.hasClosedForm();
+    }
+
+    // Exact pricer
+    std::optional<double> ExactPrice::price(const Option& opt) const
+    {
+        if (!isAvailable(opt))
+        {
+            return std::nullopt;
+        }
+
+        // Uses the black scholes pricing formula
+        return opt.price();
+    }
+
+}
--- OptionFunctions/ExactPrice.hpp
+++ OptionFunctions/ExactPrice.hpp
@@ -3,3 +3,36 @@
 //  Derived from Pricing method base class
 //
 //
+
+#ifndef EXACTPRICE_HPP
+#define EXACTPRICE_HPP
+
+#include "Option.hpp"
+#include "PricingMethod.hpp"
+#include <string>
+#include <optional>
+
+namespace Jason::Finance
+{
+    class ExactPrice : public PricingMethod
+    {
+        private:
+            ///////////////////////////////////////////////////////////////////////////////////////////////
+
+        public: 
+            ExactPrice();                                                       // Default constructor
+            ExactPrice(const ExactPrice& source);                               // Copy constructor
+            ExactPrice& operator = (const ExactPrice& source);                  // Assignment operator
+            ~ExactPrice() override;
+
+            std::string name() const override;                                  // Returns name of pricing method
+            std::optional<double> price(const Option& opt) const override;      // Returns price of pricing method
+            bool isAvailable(const Option& opt) const override;                 // Returns if pricing method is available
+
+    };
+
+
+}    // namespace Jason::Finance
+
+
+#endif  // EXACTPRICE_HPP
