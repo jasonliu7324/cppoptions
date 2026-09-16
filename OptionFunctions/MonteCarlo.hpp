92b23c04ac761191579db516befe61f9f26b9fe7
jasonliu7324 <jasonliu7324@gmail.com>
2026-09-15 20:31:12 -0400

    More on Monte Carlo

--- OptionFunctions/MonteCarlo.hpp
+++ OptionFunctions/MonteCarlo.hpp
@@ -3,3 +3,36 @@
 //  Derived from PricingMethod
 //
 //
+
+#ifndef MONTECARLO_HPP
+#define MONTECARLO_HPP
+
+#include "PricingMethod.hpp"
+#include "Option.hpp"
+#include <optional>
+#include <string>
+
+namespace Jason::Finance
+{
+    class MonteCarlo : public PricingMethod
+    {
+        private: 
+            long NSIM;          // Number of simulations
+            long NT;            // Number of time steps
+
+        public:
+            MonteCarlo();                                               // Default constructor
+            MonteCarlo(double NSIM, double NT);                         // Specific construcotr
+            MonteCarlo(const MonteCarlo& source);                       // Copy constructor
+            MonteCarlo& operator = (const MonteCarlo& source);          // Assignment operator
+            ~MonteCarlo();                                              // Default destructor
+
+            std::string name() const;
+            std::optional<double> price() const;
+            bool isAvailable(const Option& opt) const;
+    };
+
+
+}   // namespace Jason::Finance
+
+#endif // MONTECARLO_HPP
